9a14ba8ba9b75aed1da85de0552f66a7f706c09f
aesthetic-what <ladnoladno477@gmail.com>
2024-11-20 10:40:15 +0500

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,18 +1,10 @@
 import sqlite3
 import random
 
-conn = sqlite3.connect('not_telegram.db')
+conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
-# cursor.execute("""CREATE TABLE IF NOT EXISTS Products (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-#                                                     title TEXT NOT NULL,
-#                                                     description TEXT,
-#                                                     price INTEGER NOT NULL)""")
 
-# for num in range(1,5):
-#     title = f'Product{num}'
-#     description = f'Description{num}'
-#     price = random.randint(100, 1000)
-#     cursor.execute("""INSERT INTO Products (title, description, price) VALUES (?, ?, ?)""", (title, description, price))
+
 
 
 # for num in range(1, 11):
@@ -22,13 +14,47 @@ cursor = conn.cursor()
 
 # cursor.execute("DELETE FROM Users WHERE (id+2)%3 = ?", (0,))
 
-data = cursor.execute("""SELECT title, description, price FROM Products""").fetchall()
-for row in data:
-    username, email, age = row
-    print(f'title: {username} | description: {email} | price: {age}')
-conn.commit()
+# data = cursor.execute("""SELECT title, description, price FROM Products""").fetchall()
+# for row in data:
+#     username, email, age = row
+#     print(f'title: {username} | description: {email} | price: {age}')
+# conn.commit()
 
+
+    
+def initiate_db():
+    with conn:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                                    username TEXT NOT NULL,
+                                                    email TEXT NOT NULL,
+                                                    age INTEGER,
+                                                    balance INTEGER NOT NULL)""")
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Products (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                                               title TEXT NOT NULL,
+                                                               description TEXT,
+                                                               price INTEGER NOT NULL)""")
 
 def get_all_products():
     with conn:
-        return cursor.execute("""SELECT title, description, price FROM Products""").fetchall()
+        return cursor.execute("""SELECT title, description, price FROM Products""").fetchone()
+
+def add_user(username, email, age):
+    with conn:
+        return cursor.execute("""INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)""",(username, email, age, 1000))
+    
+def is_include(username):
+    check_user = cursor.execute("""SELECT * FROM Users WHERE username=(?)""", (username, )).fetchone()
+    if check_user is None:
+        return False
+    conn.commit()
+    return True
+
+# initiate_db()
+
+# for num in range(1,5):
+#     title = f'Product{num}'
+#     description = f'Description{num}'
+#     price = random.randint(100, 1000)
+#     print(title, description, price)
+#     cursor.execute("""INSERT INTO Products (title, description, price) VALUES (?, ?, ?)""", (title, description, price))
+# conn.commit()
